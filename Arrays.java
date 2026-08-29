6b733bb14c145da8333e8ff874558784047c6ee1
Juanvbit <juanvelasquezz07@hotmail.com>
2026-08-29 17:23:49 -0500

    Agrega Arrays y modifica Polinomios

---
+++ Arrays.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        try (Scanner teclado = new Scanner(System.in)) {
+
+            System.out.print("Ingrese el tamaño del arreglo: ");
+
+            int tam = teclado.nextInt();
+
+            int[] array = new int[tam];
+
+            System.out.println("Ingrese los elementos del arreglo:");
+            for (int i = 0; i < tam; i++) {
+
+                array[i] = teclado.nextInt();
+            }
+
+            System.out.println("Elementos del arreglo son:");
+            for (int i = 0; i < tam; i++) {
+                System.out.print(array[i] + " ");
+            }
+
+
+        }
+    }
+}
